36b366f371ded79c7deb4dfe32e434678a08f02e
vinceHagley <91168012+vinceHagley@users.noreply.github.com>
2025-05-06 11:30:28 +1200

    Create maze.py
    Basic template for command line game

---
+++ maze.py
@@ -0,0 +1,33 @@
+″″″ Title:  
+Author:  
+Date: 
+Version:  
+Purpose:    
+″″″ 
+avitar = (1,2,'A') #row colum Representation
+goal = (1,3,'G') #row colum Representation
+MAZE = [[1,1,1,1,1],[1,0,0,0,1],[1,1,1,1,1]]
+def print_maze(maze,avitar,goal):
+  """ prints out maze to standard io
+      -----
+      - AG-
+      -----
+  """    
+  #add code to print out maze
+  pass
+  
+def move(avi,maze):
+  """ change possition of avitar
+  """   
+  #add code to move  avitar
+  pass
+
+def play_game(avitar,goal,MAZE):
+  """ main game loop
+  """ 
+  while not(avitar[0] == goal[0] and avitar[0] == goal[0]):
+    pass
+  print("congratulations")
+  
+if __name__ == "__main__":
+  play_game(avitar,goal,MAZE)
